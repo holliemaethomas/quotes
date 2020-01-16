7ba928aa30bdf32308e09ed9040071be1aaf2310
holliemaethomas <holliemaethomas85@gmail.com>
2020-01-16 14:07:59 -0800

    refactored main to add test and added random functionality

--- src/test/java/quotes/AppTest.java
+++ src/test/java/quotes/AppTest.java
@@ -4,11 +4,17 @@
 package quotes;
 
 import org.junit.Test;
+
+import java.io.FileNotFoundException;
+
 import static org.junit.Assert.*;
 
 public class AppTest {
-    @Test public void testAppHasAGreeting() {
-        App classUnderTest = new App();
-        assertNotNull("app should have a greeting", classUnderTest.getGreeting());
+
+    @Test(expected = FileNotFoundException.class)
+    public void testPathToSeeIfExists () throws FileNotFoundException {
+        GetQuotes.randomQuote("asdasdasd");
     }
 }
+
+
